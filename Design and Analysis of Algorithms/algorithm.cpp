7a4d68ee94071fba6faf790dab2c244b0b646430
Kaloyan Dimov <kaloyandimov99@gmail.com>
2024-04-12 14:32:23 +0300

    Add naïve sorting algorithms

---
+++ Design and Analysis of Algorithms/algorithm.cpp
@@ -0,0 +1,52 @@
+//
+//  algorithm.cpp
+//  Design and Analysis of Algorithms
+//
+//  Created by Kaloyan Dimov on 12.04.24.
+//
+
+#include "algorithm.hpp"
+
+#include "utility.hpp"
+
+void insertion_sort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        
+        arr[j + 1] = key;
+    }
+}
+
+void selection_sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[i]) {
+                swap(arr[j], arr[i]);
+            }
+        }
+    }
+}
+
+void bubble_sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+        
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                
+                swapped = true;
+            }
+        }
+        
+        if (!swapped) {
+            break;
+        }
+    }
+}
---
+++ Design and Analysis of Algorithms/algorithm.hpp
@@ -0,0 +1,17 @@
+//
+//  algorithm.hpp
+//  Design and Analysis of Algorithms
+//
+//  Created by Kaloyan Dimov on 12.04.24.
+//
+
+#ifndef algorithm_hpp
+#define algorithm_hpp
+
+void insertion_sort(int arr[], int n);
+
+void selection_sort(int arr[], int n);
+
+void bubble_sort(int arr[], int n);
+
+#endif /* algorithm_hpp */
